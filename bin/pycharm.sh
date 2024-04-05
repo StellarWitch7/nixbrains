5afa224ca3fae9a8d159b32216875d1d813ec3de
Aurora Dawn <131844170+StellarWitch7@users.noreply.github.com>
2024-04-04 22:40:03 -0400

    Create pycharm.sh

---
+++ bin/pycharm.sh
@@ -0,0 +1,9 @@
+#/usr/bin/env bash
+
+pycharm_edition="pycharm-community"
+
+if command -v pycharm-professional &> /dev/null; then
+  pycharm_edition="pycharm-professional"
+fi
+
+nix-shell ~/.env/python3.nix --run $pycharm_edition
